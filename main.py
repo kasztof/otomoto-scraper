0ed6c0e39fdf37152ccfecf1f4b4743a7e9df3e9
kasztof <237986@student.pwr.edu.pl>
2018-02-04 14:38:06 +0100

    adjusting to GUI

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ import openpyxl
 from openpyxl.chart import BarChart
 
 CARS = 'https://www.otomoto.pl/osobowe/skoda/octavia/?page='
+MAXIMUM_MILEAGE = 2000000
 
 
-def add_dictionaries(dict1, dict2):
+def merge_dictionaries(dict1, dict2):
     result = defaultdict(list)
     for key, value in dict1.items():
         if key in dict2:
@@ -25,19 +26,13 @@ def add_dictionaries(dict1, dict2):
     return result
 
 
-def merge_dictionaries(dict1, dict2):
-    result = {key: [*value, *dict2[key]] if key in dict2 else value
-              for key, value in dict1.items()}
-    return result
-
-
 def merge_list_of_dictionaries(dict_list):
     for i in range(1, len(dict_list)):
-        dict_list[0] = add_dictionaries(dict_list[0], dict_list[i])
+        dict_list[0] = merge_dictionaries(dict_list[0], dict_list[i])
     return dict_list[0]
 
 
-def count_values_in_dict(dictionary):
+def number_of_values_in_dict(dictionary):
     sum_of_values = 0
     for values in dictionary.values():
         for _ in values:
@@ -53,9 +48,9 @@ def get_mileages_and_years(url):
     result_dict = defaultdict(list)
 
     for yrs, mlg in list(zip(years, mileages)):
-        mileage = int(get_number(mlg.span.string))
-        if mileage < 1000000:
-            result_dict[yrs.span.string].append(get_number(mlg.span.string))
+        mileage = get_number(mlg.span.string)
+        if mileage < MAXIMUM_MILEAGE:
+            result_dict[yrs.span.string].append(mileage)
 
     return result_dict
 
@@ -65,24 +60,25 @@ def get_number(string):
     for char in string:
         if char.isdigit():
             result += char
-    return result
+    return int(result)
 
 
-def get_car_data():
+def get_data_from_pages(start_page, end_page):
     list_of_pages = []
-    for page_number in range(2, 50):
+    for page_number in range(start_page, end_page):
         print(page_number)
         list_of_pages.append(get_mileages_and_years(CARS + str(page_number)))
 
     data_set = merge_list_of_dictionaries(list_of_pages)
+    save_to_excel(data_set)
     return data_set
 
 
 def dict_with_avg_of_values(dictionary):
     result = {}
-    for key, values in dictionary.items():
-        values = [int(i) for i in values]
-        avg = statistics.mean(values)
+    for key, val in dictionary.items():
+        # val = [int(i) for i in val]
+        avg = statistics.mean(val)
         result[key] = avg
 
     return result
@@ -90,9 +86,9 @@ def dict_with_avg_of_values(dictionary):
 
 def dict_with_median_of_values(dictionary):
     result = {}
-    for key, values in dictionary.items():
-        values = [int(i) for i in values]
-        median = statistics.median(values)
+    for key, val in dictionary.items():
+        # val = [int(i) for i in val]
+        median = statistics.median(val)
         result[key] = median
 
     return result
@@ -100,45 +96,76 @@ def dict_with_median_of_values(dictionary):
 
 def dict_with_median_and_avg_of_values(dictionary):
     result = defaultdict(list)
-    for key, values in dictionary.items():
-        values = [int(i) for i in values]
-        median = statistics.median(values)
-        avg = statistics.mean(values)
+    for key, val in dictionary.items():
+        # val = [int(i) for i in val]
+        median = statistics.median(val)
+        avg = statistics.mean(val)
         result[key].append(median)
         result[key].append(avg)
 
     return result
 
 
-DATA = collections.OrderedDict(sorted(dict_with_median_and_avg_of_values(get_car_data()).items()))
+def save_to_excel(data):
+    to_save = collections.OrderedDict(sorted(dict_with_median_and_avg_of_values(data).items()))
+    workbook = openpyxl.load_workbook('data.xlsx')
+    if not workbook['skoda_octavia']:
+        workbook.create_sheet('skoda_octavia')
+    print(workbook.active)
+    sheet = workbook['skoda_octavia']
+    print(workbook.sheetnames)
 
-workbook = openpyxl.load_workbook('data.xlsx')
-if not workbook['skoda_octavia']:
-    workbook.create_sheet('skoda_octavia')
-print(workbook.active)
-sheet = workbook['skoda_octavia']
-print(workbook.sheetnames)
+    i = 1  # no to to wez popraw gosciu
+    for key, value in to_save.items():
+        sheet.append([key, *value])
+        # sheet['B' + str(i)] = int(el)
+        # sheet['C' + str(i)] = DATA[el]
+        # i += 1
 
-i = 1  # no to to wez popraw gosciu
-for key, value in DATA.items():
-    sheet.append([key, *value])
-    # sheet['B' + str(i)] = int(el)
-    # sheet['C' + str(i)] = DATA[el]
-    # i += 1
+    chart = BarChart()
+    chart.type = "col"
+    chart.y_axis.title = 'Mileage'
+    chart.x_axis.title = 'Year of prod.'
 
-chart = BarChart()
-chart.type = "col"
-chart.y_axis.title = 'Mileage'
-chart.x_axis.title = 'Year of prod.'
+    data = openpyxl.chart.Reference(sheet, min_col=2, max_col=3, min_row=1, max_row=len(to_save))
+    cats = openpyxl.chart.Reference(sheet, min_col=1, min_row=1, max_row=len(to_save))
 
-data = openpyxl.chart.Reference(sheet, min_col=2, max_col=3, min_row=1, max_row=len(DATA))
-cats = openpyxl.chart.Reference(sheet, min_col=1, min_row=1, max_row=len(DATA))
+    chart.add_data(data)
+    chart.set_categories(cats)
+    sheet.add_chart(chart, "E2")
 
-chart.add_data(data)
-chart.set_categories(cats)
-sheet.add_chart(chart, "E2")
+    workbook.save('data.xlsx')
 
-workbook.save('data.xlsx')
+
+#DATA = collections.OrderedDict(sorted(dict_with_median_and_avg_of_values(get_data_from_pages(2, 3)).items()))
+#
+# workbook = openpyxl.load_workbook('data.xlsx')
+# if not workbook['skoda_octavia']:
+#     workbook.create_sheet('skoda_octavia')
+# print(workbook.active)
+# sheet = workbook['skoda_octavia']
+# print(workbook.sheetnames)
+#
+# i = 1  # no to to wez popraw gosciu
+# for key, value in DATA.items():
+#     sheet.append([key, *value])
+#     # sheet['B' + str(i)] = int(el)
+#     # sheet['C' + str(i)] = DATA[el]
+#     # i += 1
+#
+# chart = BarChart()
+# chart.type = "col"
+# chart.y_axis.title = 'Mileage'
+# chart.x_axis.title = 'Year of prod.'
+#
+# data = openpyxl.chart.Reference(sheet, min_col=2, max_col=3, min_row=1, max_row=len(DATA))
+# cats = openpyxl.chart.Reference(sheet, min_col=1, min_row=1, max_row=len(DATA))
+#
+# chart.add_data(data)
+# chart.set_categories(cats)
+# sheet.add_chart(chart, "E2")
+#
+# workbook.save('data.xlsx')
 
 # plt.rcParams.update({'font.size': 7})
 # plt.bar(DATA.keys(), dict_with_avg_of_values(DATA).values(), color='g')
